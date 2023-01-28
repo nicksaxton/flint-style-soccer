157778e75169c5bdd62fe728e046fa18a57b5f16
Nick Saxton <nick.saxton@gmail.com>
2023-01-28 13:59:38 -0500

    Initial layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import logo from '$lib/assets/logo.jpg';
+</script>
+
+<nav class="navbar">
+	<div class="container">
+		<a class="navbar-brand" href="#">
+			<img class="logo" src={logo} alt="Flint Style Soccer logo" />
+		</a>
+	</div>
+</nav>
+
+<div class="flex-grow-1">
+	<slot />
+</div>
+
+<footer class="text-center py-3 text-muted">
+	<small>&copy; 2023 Flint Style Soccer</small>
+</footer>
+
+<style scoped>
+	.logo {
+		height: auto;
+		width: 96px;
+	}
+</style>

4a6c3ff25393cc094d67b854e685010cb52235a1
Nick Saxton <nick.saxton@gmail.com>
2023-08-31 21:59:16 -0400

    Formatting

--- src/routes/[slug=news]/+page.server.ts
+++ src/routes/[slug=news]/+page.server.ts
@@ -1,17 +1,17 @@
 import { getPosts } from '$lib/cms';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
-  const data = await getPosts();
+export const load: PageServerLoad = async () => {
+	const data = await getPosts();
 
-  if (data) {
-    return {
-      posts: data
-    };
-  } else {
-    return {
-      status: 500,
-      body: new Error('Internal Server Error')
-    };
-  }
+	if (data) {
+		return {
+			posts: data
+		};
+	} else {
+		return {
+			status: 500,
+			body: new Error('Internal Server Error')
+		};
+	}
 };

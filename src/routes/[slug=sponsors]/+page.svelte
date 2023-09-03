908e18e9b4e5c151ea2f7d5dd4f2642d106c876e
Nick Saxton <nick.saxton@gmail.com>
2023-09-03 14:49:05 -0400

    Fix sponsor styling

--- src/routes/[slug=sponsors]/+page.svelte
+++ src/routes/[slug=sponsors]/+page.svelte
@@ -9,9 +9,9 @@
 <div class="px-4 md:px-0">
 	<HeadingBlock>Sponsors</HeadingBlock>
 
-	<div class="flex flex-wrap items-center justify-center gap-16">
+	<div class="my-12 flex flex-wrap items-center justify-center gap-12">
 		{#each sponsors as sponsor}
-			<img class="max-h-48 max-w-xs" src={sponsor.imageUrl} alt={sponsor.name} />
+			<img class="max-h-48 max-w-[240px]" src={sponsor.imageUrl} alt={sponsor.name} />
 		{/each}
 	</div>
 </div>
